119aada82899ef0d990b02c868d18ad4e8882a7b
Francesco Grillea <72458739+francescogrillea@users.noreply.github.com>
2022-01-07 15:50:49 +0100

    comparison between results

--- test.py
+++ test.py
@@ -1,24 +1,26 @@
-import datetime
-
-import numpy as np
 import pandas as pd
 
-def compare_predictions(config_path='config.yaml', results_path_a='results/', results_path_b= 'results/'):
 
-    tf_file = pd.read_csv(results_path_a)
-    tfLite_file = pd.read_csv(results_path_b)
-    tf_values = tf_file.values[0]
-    tfLite_values = tfLite_file.values[0]
-    header = tf_file.columns.values
+def compare_csv(paths):
+
+    files = []
+    for p in paths:
+        files.append(pd.read_csv(p))
+
+    header = files[0].columns.values
 
     for i in range(header.size):
         print(header[i])
-        print('\tTF : {}'.format(tf_values[i]))
-        print('\tTFL: {}'.format(tfLite_values[i]))
+        for j in range(len(files)):
+            print('\t{}: {}'.format(j, files[j].values[0][i]))
 
 
 
 if __name__ == '__main__':
-    a_path = 'results/LSTM_1_TF_results.csv'
-    b_path = 'results/LSTM_1_TFLite_results.csv'
-    compare_predictions(results_path_a=a_path, results_path_b=b_path)
+    paths = []
+    paths.append('results/test.csv')
+    paths.append('results/ESN_1_TFLite_results.csv')
+    paths.append('results/LSTM_1_TF_results.csv')
+
+
+    compare_csv(paths)
