58fda1112d6d01484d1749578ccaff9bb32619d5
Francesco Grillea <72458739+francescogrillea@users.noreply.github.com>
2022-01-12 17:53:02 +0100

    comparison between results improved

--- test.py
+++ test.py
@@ -3,23 +3,30 @@ import pandas as pd
 
 def compare_csv(paths):
 
-    files = []
+    dataframes = []
     for p in paths:
-        files.append(pd.read_csv(p))
+        dataframes.append(pd.read_csv(p))
 
-    header = files[0].columns.values
+    header = dataframes[0].columns.values
+    n_columns = dataframes[0].iloc[0].size
+    n_rows = len(dataframes[0])
 
-    for i in range(header.size):
-        print(header[i])
-        for j in range(len(files)):
-            print('\t{}: {}'.format(j, files[j].values[0][i]))
+    for i in range(n_rows):
+        print('Channel {}'.format(dataframes[0].iloc[i][0]))
+
+        for k in range(1, len(header)):
+            print('\t{}'.format(header[k]))
+            for j in range(len(dataframes)):
+                row = dataframes[j].iloc[i]
+                print('\t\t{}'.format(row[k]))
+
+        print('==========================================')
 
 
 
 if __name__ == '__main__':
     paths = []
-    paths.append('results/test.csv')
-    paths.append('results/ESN_1_TFLite_results.csv')
+    paths.append('results/LSTM_1_TFLite_results.csv')
     paths.append('results/LSTM_1_TF_results.csv')
 
 
