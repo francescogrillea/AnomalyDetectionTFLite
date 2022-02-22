ad005b5ace3b15a0a0b9b127d48145df52f46c13
Francesco Grillea <72458739+francescogrillea@users.noreply.github.com>
2022-02-22 17:56:02 +0100

    Add files via upload

---
+++ execute_5times.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TIMES=5
+
+for i in $(seq 1 $TIMES); do
+  echo "###############"
+  echo "Iteration ${i} / ${TIMES}"
+  echo "###############"
+  echo ""
+  python example.py -l labeled_anomalies.csv
+  echo ""
+  echo ""
+  echo "";
+done
+
+python merge_results.py results/
